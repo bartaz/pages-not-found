1fd69a468c76cbb5767960e046b83a534f5f3c7b
Bartek Szopka <bartek.szopka@canonical.com>
2020-08-26 21:01:36 +0200

    Rename book data fields

--- src/book.js
+++ src/book.js
@@ -14,49 +14,48 @@ function renderPage(page) {
   var isToc = page == 'toc';
   if (isToc) {
     page = {
-      choices: Object.keys(pages)
+      next: Object.keys(pages)
     }
   } else {
     page = pages[page];
   }
 
-  var theEnd = !page.choices;
+  var theEnd = !page.next;
 
   if (page.text) {
     textEl.innerHTML = '<p>' + page.text.replace(/\n/g,'<p>');
 
     if (theEnd) {
-      textEl.innerHTML += "<p><a href='#' data-link='toc'>THE END</a>";
+      textEl.innerHTML += "<p><a href='#' data-next='toc'>THE END</a>";
     }
   } else {
     textEl.innerHTML = "";
   }
 
-  if (page.choices) {
-    choicesEl.innerHTML = page.choices.map(function(choice) {
-      var id = "" + choice;
+  if (page.next) {
+    choicesEl.innerHTML = page.next.map(function(id) {
       var link = pages[id];
       var page = Object.keys(pages).indexOf(id);
-      var isAvailable = !link.requires || inventory.indexOf(link.requires) >= 0;
+      var isAvailable = !link.need || inventory.indexOf(link.need) >= 0;
       var isFound = pagesFound.indexOf(id) >= 0;
 
       if (isToc && !isFound) {
         return '<li>Page not found' + '... [' + page + ']</a></li>';
       } else if (isAvailable || isToc) {
-        return '<li><a href="#" data-link="' + id +'">' + link.title + '... [' + page + ']</a></li>';
+        return '<li><a href="#" data-next="' + id +'">' + link.clip + '... [' + page + ']</a></li>';
       } else {
-        return '<li>' + link.title + '...</li>'
+        return '<li>' + link.clip + '...</li>'
       }
     }).join("");
   } else {
     choicesEl.innerHTML = "";
   }
 
-  if (page.add) {
-    inventory.push(page.add)
+  if (page.gain) {
+    inventory.push(page.gain)
   }
-  if (page.remove) {
-    page.remove.forEach(function(item) {
+  if (page.lose) {
+    page.lose.forEach(function(item) {
       var index = inventory.indexOf(item);
       if (index > -1) {
         inventory.splice(index, 1);
@@ -79,7 +78,7 @@ renderPage('intro');
 
 document.addEventListener('click', function(event) {
 	var target = event.target;
-  if (target.dataset.link) {
-    renderPage(target.dataset.link)
+  if (target.dataset.next) {
+    renderPage(target.dataset.next)
   }
 });
